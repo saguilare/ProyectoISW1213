0848586af2e45e205038128f8eedcf1041077079
steven aguilar <steven@stevens-MacBook-Pro.local>
2018-08-12 14:46:40 -0600

    update agregar'

--- app/src/main/java/org/example/proyectoisw1213/AgregarActivity.java
+++ app/src/main/java/org/example/proyectoisw1213/AgregarActivity.java
@@ -1,10 +1,16 @@
 package org.example.proyectoisw1213;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
@@ -12,17 +18,35 @@ import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
 
+import java.io.FileNotFoundException;
+
 public class AgregarActivity extends AppCompatActivity {
 
     int PLACE_PICKER_REQUEST =1;
+    int PHOTO_PICKER_REQUEST =2;
+
     EditText ubicacion;
+    Button btnCargarImagen;
+    RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar);
 
-        ubicacion = (EditText) findViewById(R.id.etUbicacion);
+        ubicacion = (EditText) findViewById(R.id.etGps);
+        btnCargarImagen = (Button)findViewById(R.id.btnAgregarFoto);
+
+
+    }
+
+    public void seleccionarFoto(View view){
+        try{
+            Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, PHOTO_PICKER_REQUEST);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
 
@@ -43,6 +67,19 @@ public class AgregarActivity extends AppCompatActivity {
             if(codigoResult == RESULT_OK){
                 Place place = PlacePicker.getPlace(AgregarActivity.this,data);
                 ubicacion.setText(place.getAddress());
+            }
+        }else if(codigoRequest == PHOTO_PICKER_REQUEST){
+            Uri targetUri = data.getData();
+            EditText etFoto = (EditText)findViewById(R.id.etFoto);
+            etFoto.setText(targetUri.toString());
+            Bitmap bitmap;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                ImageView ivFoto = (ImageView)findViewById(R.id.ivFoto);
+                ivFoto.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
         }
     }
