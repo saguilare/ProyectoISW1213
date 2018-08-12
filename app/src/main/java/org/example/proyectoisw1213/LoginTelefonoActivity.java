00bcff3ea705a611835dafc43b9ae40021566487
estebanvcastro <estebanvcastro@gmail.com>
2018-08-11 21:18:25 -0600

    Login Telefono

--- app/src/main/java/org/example/proyectoisw1213/LoginTelefonoActivity.java
+++ app/src/main/java/org/example/proyectoisw1213/LoginTelefonoActivity.java
@@ -1,13 +1,119 @@
 package org.example.proyectoisw1213;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginTelefonoActivity extends AppCompatActivity {
+
+    private EditText etxtPhone;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText etxtPhoneCode;
+    private String mVerificationId;
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_telefono);
+
+        etxtPhone = (EditText) findViewById(R.id.etNum);
+        etxtPhoneCode = (EditText) findViewById(R.id.etCod);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null) {
+                    startActivity(new Intent(LoginTelefonoActivity.this,NavDrawerActivity.class));
+                }
+            }
+        };
     }
+
+    public void requestCode(View view) {
+        String phoneNumber = etxtPhone.getText().toString();
+        if (TextUtils.isEmpty(phoneNumber))
+            return;
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber, 60, TimeUnit.SECONDS, LoginTelefonoActivity.this, new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+                        //Called if it is not needed to enter verification code
+                        signInWithCredential(phoneAuthCredential);
+                    }
+
+                    @Override
+                    public void onVerificationFailed(FirebaseException e) {
+                        //incorrect phone number, verification code, emulator, etc.
+                        Toast.makeText(LoginTelefonoActivity.this, "onVerificationFailed " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCodeSent(String verificationId, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        //now the code has been sent, save the verificationId we may need it
+                        super.onCodeSent(verificationId, forceResendingToken);
+
+                        mVerificationId = verificationId;
+
+                    }
+
+                    @Override
+                    public void onCodeAutoRetrievalTimeOut(String verificationId) {
+                        //called after timeout if onVerificationCompleted has not been called
+                        super.onCodeAutoRetrievalTimeOut(verificationId);
+                        Toast.makeText(LoginTelefonoActivity.this, "onCodeAutoRetrievalTimeOut :" + verificationId, Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
+
+    private void signInWithCredential(PhoneAuthCredential phoneAuthCredential) {
+        mAuth.signInWithCredential(phoneAuthCredential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginTelefonoActivity.this, "Autenticacion Exitosa", Toast.LENGTH_SHORT).show();
+
+
+                        } else {
+                            Toast.makeText(LoginTelefonoActivity.this, "Autenticacion Fallida" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void signIn(View view) {
+        String code = etxtPhoneCode.getText().toString();
+        if (TextUtils.isEmpty(code))
+            return;
+
+        signInWithCredential(PhoneAuthProvider.getCredential(mVerificationId, code));
+    }
+
+
+
 }
--- app/src/main/java/org/example/proyectoisw1213/NavDrawerActivity.java
+++ app/src/main/java/org/example/proyectoisw1213/NavDrawerActivity.java
@@ -1,5 +1,6 @@
 package org.example.proyectoisw1213;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class NavDrawerActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +43,11 @@ public class NavDrawerActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Modificacion
+        FirebaseAuth.getInstance().signOut();
+
+        startActivity(new Intent(NavDrawerActivity.this,MainActivity.class));
     }
 
     @Override
